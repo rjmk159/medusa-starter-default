19d5e10b900766d9f2d265d6ef61767c84a48589
Raja Khan <proservz@Proservz-DELSpare-Pro1s-MacBook-Pro.local>
2022-10-13 03:02:02 +0530

    nnew things

--- medusa-config.js
+++ medusa-config.js
@@ -1,36 +1,36 @@
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 
-let ENV_FILE_NAME = '';
+let ENV_FILE_NAME = "";
 switch (process.env.NODE_ENV) {
-	case 'production':
-		ENV_FILE_NAME = '.env.production';
+	case "production":
+		ENV_FILE_NAME = ".env.production";
 		break;
-	case 'staging':
-		ENV_FILE_NAME = '.env.staging';
+	case "staging":
+		ENV_FILE_NAME = ".env.staging";
 		break;
-	case 'test':
-		ENV_FILE_NAME = '.env.test';
+	case "test":
+		ENV_FILE_NAME = ".env.test";
 		break;
-	case 'development':
+	case "development":
 	default:
-		ENV_FILE_NAME = '.env';
+		ENV_FILE_NAME = ".env";
 		break;
 }
 
 try {
-	dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME });
-} catch (e) {
-}
+	dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+} catch (e) {}
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS =
+	process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 // Database URL (here we use a local database called medusa-development)
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-store";
+	process.env.DATABASE_URL || "postgres://localhost/medusa-store";
 
 // Medusa uses Redis, so this needs configuration as well
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
@@ -41,29 +41,39 @@ const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
 
 // This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
-  `medusa-fulfillment-manual`,
-  `medusa-payment-manual`,
-  // Uncomment to add Stripe support.
-  // You can create a Stripe account via: https://stripe.com
-  // {
-  //   resolve: `medusa-payment-stripe`,
-  //   options: {
-  //     api_key: STRIPE_API_KEY,
-  //     webhook_secret: STRIPE_WEBHOOK_SECRET,
-  //   },
-  // },
+	`medusa-fulfillment-manual`,
+	`medusa-payment-manual`,
+	{
+		resolve: `medusa-file-s3`,
+		options: {
+			s3_url: "https://ghazalibucket.s3.us-east-1.amazonaws.com",
+			bucket: "ghazalibucket",
+			region: "us-east-1",
+			access_key_id: "AKIA2MR44HAAAVAWPRUE",
+			secret_access_key: "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",
+		},
+	},
+	// Uncomment to add Stripe support.
+	// You can create a Stripe account via: https://stripe.com
+	// {
+	//   resolve: `medusa-payment-stripe`,
+	//   options: {
+	//     api_key: STRIPE_API_KEY,
+	//     webhook_secret: STRIPE_WEBHOOK_SECRET,
+	//   },
+	// },
 ];
 
 module.exports = {
-  projectConfig: {
-    // redis_url: REDIS_URL,
-    // For more production-like environment install PostgresQL
-    // database_url: DATABASE_URL,
-    // database_type: "postgres",
-    database_database: "./medusa-db.sql",
-    database_type: "sqlite",
-    store_cors: STORE_CORS,
-    admin_cors: ADMIN_CORS,
-  },
-  plugins,
+	projectConfig: {
+		// redis_url: REDIS_URL,
+		// For more production-like environment install PostgresQL
+		// database_url: DATABASE_URL,
+		// database_type: "postgres",
+		database_url: DATABASE_URL,
+		database_type: "postgres",
+		// store_cors: STORE_CORS,
+		// admin_cors: ADMIN_CORS,
+	},
+	plugins,
 };
